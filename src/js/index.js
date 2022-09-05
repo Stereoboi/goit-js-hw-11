012a03ddf08d26cb9e448c8f20ca8a6fa97ed54d
Bohdan Pishchak <3.14shcha@gmail.com>
2022-09-05 14:44:59 +0300

    r

--- src/js/index.js
+++ src/js/index.js
@@ -134,7 +134,7 @@ function infinityScroll() {
       Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
   }
   console.log(window.innerHeight);
-  console.log(documentRect.bottom);
+  console.log(Math.ceil(documentRect.bottom));
   console.log(window.innerHeight === Math.ceil(documentRect.bottom));
 
 }
